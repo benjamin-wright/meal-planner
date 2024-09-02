e8789d110542113cebaeb5b60d2a672f34cd2683
Ben Wright <benjamin.p.wright@hotmail.com>
2024-09-02 17:00:12 +0100

    figuring out color palettes

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home } from "./pages";
 import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Home />,
   },
 ]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
+  </StrictMode>
 );
