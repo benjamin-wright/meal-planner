6ace8eaf5147008d93f0f660ab87338585e99ebc
Ben Wright <benjamin.p.wright@hotmail.com>
2024-10-10 21:50:20 +0100

    different approach, using css modules

--- src/components/slide-out-link.tsx
+++ src/components/slide-out-link.tsx
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom";
 import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import classes from "./slide-out-link.module.css";
 
 interface SlideOutLinkProps {
   icon: IconDefinition;
@@ -12,19 +13,21 @@ interface SlideOutLinkProps {
 export default function SlideOutLink({ icon, content, to }: SlideOutLinkProps) {
   const [initial, setInitial] = useState(true);
 
-  const style = {
-    height: "1.5em",
-    width: `${initial ? "2em" : "auto"}`,
-    padding: `${initial ? "0.5em" : "0.5em 1em 0.5em 0.5em"}`,
-  };
+  useEffect(() => {
+    setTimeout(() => setInitial(false), 1000);
+  }, [setInitial]);
+
+  const styles = [classes.button];
+  if (!initial) {
+    styles.push(classes.slide);
+  }
 
   return (
-    <Link className="button" to={to} style={style}>
-      <span className="wrapper">
-        <FontAwesomeIcon icon={icon} className="icon" spin />
+    <Link className={styles.join(" ")} to={to}>
+      <span className={classes.wrapper}>
+        <FontAwesomeIcon icon={icon} className={classes.icon} fixedWidth />
       </span>
-      &nbsp;&nbsp;&nbsp;
-      {content}
+      <span>{content}</span>
     </Link>
   );
 }
