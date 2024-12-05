4ce1ab53d7cc9d223104d949c89944df0a270ee9
Ben Wright <bwright@roku.com>
2024-12-05 11:53:25 +0000

    make my own drawer

--- src/ui/components/nav.tsx
+++ src/ui/components/nav.tsx
@@ -1,67 +1,66 @@
 import {
   Box,
   Drawer,
-    Menu,
-    MenuItem,
-    Typography,
-  } from "@mui/material";
-  import Settings from "@mui/icons-material/Settings";
-  import Scale from "@mui/icons-material/Scale";
-  import Sell from "@mui/icons-material/Sell";
-  import Egg from "@mui/icons-material/Egg";
-  import RestaurantRounded from "@mui/icons-material/RestaurantRounded";
-  import CalendarMonth from "@mui/icons-material/CalendarMonth";
-  import { useNavigate, useLocation } from "react-router-dom";
-  import { useState } from "react";
-  import { Menu as MenuIcon } from "@mui/icons-material";
-  import { useTheme } from "@mui/material";
-  import { IconLink } from "./icon-link";
-  
-  export function Nav() {
-    const [open, setOpen] = useState(false);
-    const navigate = useNavigate();
-    const location = useLocation();
-    const theme = useTheme();
+  Menu,
+  MenuItem,
+  Typography,
+} from "@mui/material";
+import Settings from "@mui/icons-material/Settings";
+import Scale from "@mui/icons-material/Scale";
+import Sell from "@mui/icons-material/Sell";
+import Egg from "@mui/icons-material/Egg";
+import RestaurantRounded from "@mui/icons-material/RestaurantRounded";
+import CalendarMonth from "@mui/icons-material/CalendarMonth";
+import { useNavigate, useLocation } from "react-router-dom";
+import { useState } from "react";
+import { Menu as MenuIcon } from "@mui/icons-material";
+import { useTheme } from "@mui/material";
+import { IconLink } from "./icon-link";
 
-    function navigateTo(category: string) {
-      navigate(`/${category}`);
-    }
+export function Nav() {
+  const [open, setOpen] = useState(false);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const theme = useTheme();
 
-    const options = [
-      { name: "planner", icon: CalendarMonth },
-      { name: "recipies", icon: RestaurantRounded },
-      { name: "ingredients", icon: Egg },
-      { name: "categories", icon: Sell },
-      { name: "units", icon: Scale },
-      { name: "settings", icon: Settings },
-    ];
-  
-    return (
-      <>
-        <IconLink onClick={() => setOpen(true)}>
-          <MenuIcon />
-        </IconLink>
-        <Drawer anchor="right" open={open} onClose={() => setOpen(false)} PaperProps={{
-          sx: {
-            marginTop: "3.5em",
-            height: "unset",
-            bottom: "0"
-          }
-        }}>
-          {options.map((option) => (
-            <MenuItem key={option.name} onClick={() => navigateTo(option.name)} sx={{
-              padding: "1em",
-              display: "flex",
-              justifyContent: "stretch",
-            }} disabled={location.pathname === "/"+option.name}>
-              <option.icon />
-              <Typography variant="h6" sx={{
-                marginLeft: "1em",
-              }}>{option.name}</Typography>
-            </MenuItem>
-          ))}
-        </Drawer>
-      </>
-    );
+  function navigateTo(category: string) {
+    navigate(`/${category}`);
   }
-  
+
+  const options = [
+    { name: "planner", icon: CalendarMonth },
+    { name: "recipies", icon: RestaurantRounded },
+    { name: "ingredients", icon: Egg },
+    { name: "categories", icon: Sell },
+    { name: "units", icon: Scale },
+    { name: "settings", icon: Settings },
+  ];
+
+  return (
+    <>
+      <IconLink onClick={() => setOpen(true)}>
+        <MenuIcon />
+      </IconLink>
+      <Drawer anchor="right" transitionDuration={{ enter: 500000, exit: 500 }} open={open} onClose={() => setOpen(false)} PaperProps={{
+        sx: {
+          marginTop: "3.5em",
+          height: "unset",
+          bottom: "0"
+        }
+      }}>
+        {options.map((option) => (
+          <MenuItem key={option.name} onClick={() => navigateTo(option.name)} sx={{
+            padding: "1em",
+            display: "flex",
+            justifyContent: "stretch",
+          }} disabled={location.pathname === "/" + option.name}>
+            <option.icon />
+            <Typography variant="h6" sx={{
+              marginLeft: "1em",
+            }}>{option.name}</Typography>
+          </MenuItem>
+        ))}
+      </Drawer>
+    </>
+  );
+}
