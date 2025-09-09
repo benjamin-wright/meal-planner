fa388b36599b3089cce812cfaf6deaee182d443e
Ben Wright <bwright@roku.com>
2025-09-09 15:39:37 +0100

    WIP

--- src/ui/components/inputs/numeric-input/numeric-input.stories.tsx
+++ src/ui/components/inputs/numeric-input/numeric-input.stories.tsx
@@ -2,11 +2,15 @@ import { useState } from 'react';
 import { NumericInput } from './numeric-input';
 
 import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from 'storybook/test';
 
-function TestComponent() {
+function TestComponent({ fn }: { fn: (value: number) => void }) {
   const [value, setValue] = useState(0);
   return <div>
-    <NumericInput id="test" label="Test" value={value} onChange={setValue} />
+    <NumericInput id="test" label="Test" value={value} onChange={value => {
+      setValue(value);
+      fn(value);
+    }} />
     <button onClick={() => setValue(value + 1)}>Increase</button>
   </div>;
 }
@@ -19,4 +23,8 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {};
+export const Primary: Story = {
+  args: {
+    fn: fn()
+  }
+};
--- src/ui/components/inputs/numeric-input/numeric-input.tsx
+++ src/ui/components/inputs/numeric-input/numeric-input.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { Fieldset } from "../fieldset/fieldset"
 import "./numeric-input.css"
-import { fixJSRounding } from "../../../../utils/number";
 
 type Props = {
   id: string;
@@ -11,26 +10,41 @@ type Props = {
 }
 
 export function NumericInput({ id, label, value, onChange }: Props) {
-  const [innerValue, setInnerValue] = useState(value.toString());
+  const rounded = Math.round(value * 1000) / 1000;
+  const [innerValue, setInnerValue] = useState(rounded.toString());
 
   useEffect(() => {
-    setInnerValue(value.toString());
-  }, [value]);
+    setInnerValue(rounded.toString());
+  }, [rounded]);
 
   function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
     const newValue = event.target.value;
-    setInnerValue(newValue);
-
-    const parsed = fixJSRounding(parseFloat(newValue));
-    if (!isNaN(parsed) && parsed.toString() === newValue) {
-      onChange(parsed);
+    const parts = newValue.split(".");
+    if (parts.length > 2) {
+      return;
     }
+    if (parts.length === 2 && parts[1].length > 3) {
+      return;
+    }
+
+    setInnerValue(newValue);
+  }
+
+  function handleBlur() {
+    const parsed = parseFloat(innerValue);
+    if (isNaN(parsed)) {
+      return
+    }
+
+    const rounded = Math.round(parsed * 1000) / 1000;
+    setInnerValue(rounded.toString());
+    onChange(rounded);
   }
 
   return (
     <Fieldset id={id} label={label}>
       <label htmlFor={id} hidden>{label}</label>
-      <input className="numeric-input" id={id} type="number" value={innerValue} onChange={handleChange} />
+      <input className="numeric-input" id={id} type="number" value={innerValue} onChange={handleChange} onBlur={handleBlur} />
     </Fieldset>
   )
 }
