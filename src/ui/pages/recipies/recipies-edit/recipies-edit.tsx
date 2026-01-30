c517fbd30d6e9956a8a8fa102644f944a6477cdc
ben wright <benjamin.p.wright@hotmail.com>
2026-01-30 14:04:49 +0000

    recipies edit page

---
+++ src/ui/pages/recipies/recipies-edit/recipies-edit.tsx
@@ -0,0 +1,7 @@
+import { Page } from "../../../components/layout/page/page";
+
+export default function RecipiesEdit() {
+  return <Page title="Edit Recipie">
+    <p>Recipie Edit Page</p>
+  </Page>
+}
--- src/ui/pages/recipies/routes.tsx
+++ src/ui/pages/recipies/routes.tsx
@@ -1,8 +1,17 @@
+import RecipiesEdit from "./recipies-edit/recipies-edit";
 import { Recipies } from "./recipies/recipies";
 
 export const routes = [
   {
     path: "recipies",
     element: <Recipies />,
+  },
+  {
+    path: "recipies/new",
+    element: <RecipiesEdit />,
+  },
+  {
+    path: "recipies/:id",
+    element: <RecipiesEdit />,
   },
 ];
